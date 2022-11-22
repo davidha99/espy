ec56cc50c5a146ab930116f24284db24717ebd44
BernardoG <A00570682@itesm.mx>
2022-11-21 22:58:55 -0600

    Update. File can be readed from cmdln

--- src/compiler.py
+++ src/compiler.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from environment import Environment_Stack
 from parser import parser
 
@@ -19,29 +20,14 @@ def create_binary(program):
 
 
 if __name__ == '__main__':
-    # program = '''(letrec ([fib (lambda (x) (if (<= x 2) 1 (+ 0 (fib (- x 1)) (fib (- x 2)))))]) (fib 17))'''
-    # program = '''(letrec ([f (lambda (x acc) (if (zero? x) acc (f (sub1 x) (* acc x))))]) (f 5 1))'''
-    # program = '''(letrec ([f (lambda (x) (if (zero? x) 0 (+ 1 (f (sub1 x)))))]) (f 200))'''
-    # program = '''(list [arr 2 3 4 5])'''
-    # program = '''(letrec ([fib (lambda (n r1 r2) (if (== n 0) r1 (if (== n 1) r2 (fib (- n 1) r2 (+ r1 r2)))))]) (fib 6 0 1))'''
-    # program = '''(+ 2 3 ( + 4 5 ( + 6 (+ 7 8) 9) 10) 11)'''
-    # program = '''(letrec ([f (lambda (x) 
-    #                             (if (zero? x)
-    #                                 1 
-    #                                 (* x (f (sub1 x)))))]) 
-    #                 (f 5))'''
-    # program = '''(letrec ([sum (lambda (n ac)
-    #                                 (if (zero? n)
-    #                                     ac
-    #                                     (sum (sub1 n) (+ n ac))))])
-    #                 (sum 10 0))'''
-    # print(program)
-    # create_binary(program)
-    while True:
-        program = input("espy> ")
-        if program == "exit":
-            exit()
-        create_binary(program)
     
-    (letrec ([fib (lambda (x) (if (<= x 2) arr[0] (+ 0 (fib (- x arr[0])) (fib (- x 2)))))]) (fib arr[2]))
-    (letrec ([fib (lambda (x) (if (<= x y) 1 (+ 0 (fib (- x 1)) (fib (- x y)))))]) (fib arr[2]))
+    if len(sys.argv) == 2:
+        with open(sys.argv[1], 'r') as f:
+            program = f.read()
+        create_binary(program)
+    elif len(sys.argv) == 1:
+        while True:
+            program = input("espy> ")
+            if program == "exit":
+                exit()
+            create_binary(program)
